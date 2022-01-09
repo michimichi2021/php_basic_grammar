0012ff8b7e31ae203f9d29bef52c1669b9111ddb
michimichi2021 <michikaho1009@gmail.com>
2022-01-09 16:38:23 +0900

    詳解PHP基礎文法編

---
+++ main.php
@@ -0,0 +1,14 @@
+<?php
+
+// $name = 'taguchi';
+$name = 'dotinstall';
+
+
+// echo 'Hello taguchi' . PHP_EOL;
+// echo 'Hi taguchi' . PHP_EOL;
+
+echo 'Hello ' . $name . PHP_EOL;
+echo 'Hi ' . $name . PHP_EOL;
+
+
+?>
